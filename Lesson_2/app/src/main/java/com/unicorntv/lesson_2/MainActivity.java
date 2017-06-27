b4895754e9927374519956ae618a0405b1f17457
Eric Carter <eacarter@mtu.edu>
2017-06-27 19:35:32 -0400

    Lesson 2 completed

--- Lesson_2/app/src/main/java/com/unicorntv/lesson_2/MainActivity.java
+++ Lesson_2/app/src/main/java/com/unicorntv/lesson_2/MainActivity.java
@@ -2,12 +2,34 @@ package com.unicorntv.lesson_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text);
+        edit = (EditText)findViewById(R.id.edit);
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!edit.getText().equals(" ") || !edit.getText().equals(null)){
+                    text.setText(edit.getText());
+                }
+
+            }
+        });
     }
 }
